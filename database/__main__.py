b44fbfcb2ddbcd884e837190c4e141f710a1aa4f
Evgenia Karunus <lakesare@gmail.com>
2025-04-22 13:00:40 +0500

    refactor: make all env vars required (#4)
    * `.env` - use default value for "EMBEDDING_DEVICE"
    
    * `.env` - use default value for "LOG_LEVEL"
    
    * `.env` - use default value for "LOG_FILEMODE"
    
    * `.env` - use default value for "LOG_FILENAME"
    
    * `.env.example` - better comments

--- database/__main__.py
+++ database/__main__.py
@@ -8,9 +8,9 @@ from . import main
 
 dotenv.load_dotenv()
 logging.basicConfig(
-    filename=os.environ.get("LOG_FILENAME"),
-    filemode=os.environ.get("LOG_FILEMODE", "w"),
-    level=os.environ.get("LOG_LEVEL", "WARNING").upper(),
+    filename=os.environ["LOG_FILENAME"] or None,
+    filemode=os.environ["LOG_FILEMODE"],
+    level=os.environ["LOG_LEVEL"],
 )
 jixia.run.executable = os.environ["JIXIA_PATH"]
 
